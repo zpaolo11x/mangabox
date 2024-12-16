717430bd34b2ef2ba1a91d860f05f6a5e8ac8989
Paolo Zago <paolo.zago@gmail.com>
2024-12-16 20:24:23 +0000

    test close button

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
 app.commandLine.appendSwitch('enable-features', 'OverlayScrollbar');
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-	 autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		autoHideMenuBar: true,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
 
-  mainWindow.loadFile('index.html'); // Replace 'index.html' with your main HTML file name
+	mainWindow.loadFile('index.html'); // Replace 'index.html' with your main HTML file name
 });
 
 app.on('window-all-closed', () => {
-  app.quit();
+	app.quit();
 });
+
+ipcMain.on('quit-app', () => {
+	app.quit();
+});
+
+app.on('ready', createWindow);
+
