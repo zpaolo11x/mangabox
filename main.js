e50c892cc03ec34bd8decca337cccfbadbf80b39
Paolo Zago <paolo.zago@gmail.com>
2024-12-16 20:53:50 +0000

    rollback

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
 app.commandLine.appendSwitch('enable-features', 'OverlayScrollbar');
 
-function createWindow() {
-	mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		autoHideMenuBar: true,
-		webPreferences: {
-			preload: require('path').join(__dirname, 'preload.js'),
-			contextIsolation: true, // REQUIRED for contextBridge
-			enableRemoteModule: false, // Best practice for security
-			nodeIntegration: false, // Ensures better security
-			sandbox: true, // Optional: Runs preload in a secure sandbox
-		},
-	});
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+	 autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-	mainWindow.loadFile('index.html'); // Replace 'index.html' with your main HTML file name
-};
+  mainWindow.loadFile('index.html'); // Replace 'index.html' with your main HTML file name
+});
 
 app.on('window-all-closed', () => {
-	app.quit();
+  app.quit();
 });
-
-ipcMain.on('quit-app', () => {
-	app.quit();
-});
-
-app.on('ready', createWindow);
