344bafc001116d250a89d75cd2ac5ab4e1e9fa4a
Paolo Zago <paolo.zago@gmail.com>
2025-02-02 17:32:31 +0000

    selective pinch to zoom

--- main.js
+++ main.js
@@ -17,8 +17,17 @@ app.on('ready', () => {
   });
 
   mainWindow.loadFile('index.html'); // Replace 'index.html' with your main HTML file name
-  mainWindow.webContents.setVisualZoomLevelLimits(1, 5);
-
+  //mainWindow.webContents.setVisualZoomLevelLimits(1, 5);
+  // Check URL changes and enable zoom conditionally
+  mainWindow.webContents.on('did-navigate', (_, url) => {
+	if (url.includes('bookread')) {
+	  // ✅ Enable pinch-to-zoom only when URL contains "reader.html"
+	  mainWindow.webContents.setVisualZoomLevelLimits(1, 5);
+	} else {
+	  // ❌ Disable pinch-to-zoom for other pages
+	  mainWindow.webContents.setVisualZoomLevelLimits(1, 1);
+	}
+ });
 });
 
 app.on('window-all-closed', () => {
