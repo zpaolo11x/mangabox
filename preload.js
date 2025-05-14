6faef6cc998d0cccd740d509be53bd69ace876a4
Paolo Zago <paolo.zago@gmail.com>
2025-05-14 07:24:11 +0000

    app version in dragbar

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 // preload.js
 const { contextBridge, ipcRenderer } = require('electron');
+const { version } = require('package.json');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   minimize: () => ipcRenderer.send('window-minimize'),
@@ -8,5 +9,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   onFullscreenChange: (callback) => ipcRenderer.on('fullscreen-changed', (_, isFullscreen) => callback(isFullscreen)),
   onMaximize: (callback) => ipcRenderer.on('window-maximized', callback),
   onUnmaximize: (callback) => ipcRenderer.on('window-unmaximized', callback),
+  getAppVersion: () => version,
 });
 
