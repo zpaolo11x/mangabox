30353a6c10017deae17450064f1e063537cbedef
Paolo Zago <paolo.zago@gmail.com>
2025-07-01 09:35:05 +0000

    cleanup login code

--- auth.js
+++ auth.js
@@ -3,58 +3,58 @@
 // Show the login dialog if no auth header or base URL is available
 if (!localStorage.getItem('mbAuthHeader') || !localStorage.getItem('mbBaseUrl')) {
 	if (localStorage.getItem('mbBaseUrl')) {
-		document.getElementById('mbBaseUrl').value = localStorage.getItem('mbBaseUrl');
+		loginBaseUrl.value = localStorage.getItem('mbBaseUrl');
 	}
 	showLoginDialog();
 }
 
 function login() {
-	let mbBaseUrl = document.getElementById('mbBaseUrl').value;
-	const username = document.getElementById('username').value;
-	const password = document.getElementById('password').value;
-	const rememberMe = document.getElementById('rememberMe').checked;
+	let baseUrlVal = loginBaseUrl.value;
+	const usernameVal = loginUsername.value;
+	const passwordVal = loginPassword.value;
+	const rememberMeVal = loginRememberMe.checked;
 
-	if (!/^https?:\/\//i.test(mbBaseUrl)) {
+	if (!/^https?:\/\//i.test(baseUrlVal)) {
 		// Add http:// if no protocol is present
-		mbBaseUrl = 'https://' + mbBaseUrl;
+		baseUrlVal = 'https://' + baseUrlVal;
 	}
 
-	mbBaseUrl = mbBaseUrl.replace(/\/$/, '');
+	baseUrlVal = baseUrlVal.replace(/\/$/, '');
 
-	const mbAuthHeader = 'Basic ' + btoa(`${username}:${password}`);
+	const mbAuthHeader = 'Basic ' + btoa(`${usernameVal}:${passwordVal}`);
 
 	// Test the auth header and base URL with a simple API call to validate credentials
-	fetch(`${mbBaseUrl}/api/v1/login/set-cookie`, {
+	fetch(`${baseUrlVal}/api/v1/login/set-cookie`, {
 		method: 'GET',
-		headers: { 
+		headers: {
 			'Authorization': mbAuthHeader,
 			'skip_zrok_interstitial': '1'
-		 }
+		}
 	})
 		.then(response => {
 			console.log(response);
 			if (response.ok) {
-				localStorage.setItem('mbRememberMe', rememberMe);
+				localStorage.setItem('mbRememberMe', rememberMeVal);
 				localStorage.setItem('mbAuthHeader', mbAuthHeader); // Save auth header
-				localStorage.setItem('mbBaseUrl', mbBaseUrl);       // Save base URL
+				localStorage.setItem('mbBaseUrl', baseUrlVal);       // Save base URL
 				hideLoginDialog();
 				location.reload(true);
 				//fetchLibraries(); // Fetch libraries after successful login
 			} else {
-				localStorage.setItem('mbBaseUrl', mbBaseUrl);       // Save base URL
-				document.getElementById('loginError').classList.remove('auth-hidden'); // Show error message
+				localStorage.setItem('mbBaseUrl', baseUrlVal);       // Save base URL
+				loginError.classList.remove('auth-hidden'); // Show error message
 			}
 		})
 		.catch(error => {
 			console.error('Login error:', error);
-			document.getElementById('loginError').classList.remove('auth-hidden');
+			loginError.classList.remove('auth-hidden');
 		});
 }
 
 function showLoginDialog() {
-	document.getElementById('loginScreen').classList.remove('auth-hidden');
+	loginScreen.classList.remove('auth-hidden');
 }
 
 function hideLoginDialog() {
-	document.getElementById('loginScreen').classList.add('auth-hidden');
+	loginScreen.classList.add('auth-hidden');
 }
