5d1b46f2c3ad24a2a4cec26ac21e85e2c10ca1e5
Paolo Zago <paolo.zago@gmail.com>
2026-02-03 21:03:42 +0000

    fix check

--- script-auth.js
+++ script-auth.js
@@ -393,6 +393,7 @@ async function loginToServer(event, serverId, test) {
 		// so either I make login work in offline, or
 		// I need to save the user id even in this logintoserver if it's not there
 		// OR I can check if the id is there or not.
+			if (isLoginScreenHidden()) navigateTo('#dashboard');
 
 		if (mb.currentUserId == false){
 			login(serverId, test, false);
